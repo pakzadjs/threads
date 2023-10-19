4186e3344554dee88a66f297c5dccb9fa189740e
Pakzad <pakzadlive@gmail.com>
2023-10-19 19:37:57 +0200

    auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import "../globals.css";
 
 export const metadata: Metadata = {
@@ -8,8 +9,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
-  );
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
 }
