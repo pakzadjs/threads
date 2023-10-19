9be1dd35c6ca4a395fe9ed7b7d1132494d30e38f
Pakzad <pakzadlive@gmail.com>
2023-10-19 20:54:43 +0200

    Bottom bar (for mobile and tablets)

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Bottombar() {
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
