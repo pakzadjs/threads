2e0bc66c768922315712d1d2957049ca9d367a19
Pakzad <pakzadlive@gmail.com>
2023-11-05 15:56:38 +0100

    Delete thread

---
+++ components/forms/DeleteThread.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+import { deleteThread } from "@/lib/actions/thread.action";
+
+interface Props {
+  threadId: string;
+  currentUserId: string;
+  authorId: string;
+  parentId: string | null;
+  isComment?: boolean;
+}
+
+export default function DeleteThread({
+  threadId,
+  currentUserId,
+  authorId,
+  parentId,
+  isComment,
+}: Props) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  if (currentUserId !== authorId || pathname === "/") return null;
+
+  return (
+    <Image
+      src="/assets/delete.svg"
+      alt="delte"
+      width={18}
+      height={18}
+      className="cursor-pointer object-contain"
+      onClick={async () => {
+        await deleteThread(JSON.parse(threadId), pathname);
+        if (!parentId || !isComment) {
+          router.push("/");
+        }
+      }}
+    />
+  );
+}
