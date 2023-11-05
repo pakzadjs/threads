5427d03c96a9a7e27baf04b56f4bdc07828556a7
Pakzad <pakzadlive@gmail.com>
2023-11-05 17:02:12 +0100

    Bug fix

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -16,6 +16,8 @@ interface Props {
 export default function UserCard({ id, name, username, imgUrl, personType }: Props) {
   const router = useRouter();
 
+  const isCommunity = personType === "Community";
+
   return (
     <article className="user-card">
       <div className="user-card_avatar">
@@ -37,7 +39,11 @@ export default function UserCard({ id, name, username, imgUrl, personType }: Pro
       <Button
         className="user-card_btn"
         onClick={() => {
-          router.push(`/profile/${id}`);
+          if (isCommunity) {
+            router.push(`/communities/${id}`);
+          } else {
+            router.push(`/profile/${id}`);
+          }
         }}
       >
         View
