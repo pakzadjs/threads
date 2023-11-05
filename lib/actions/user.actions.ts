0ba32781b5d8a1d1c42a2c2a5adbd2a07dcb65f4
Pakzad <pakzadlive@gmail.com>
2023-11-05 13:34:52 +0100

    Activity page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -132,3 +132,29 @@ export async function fetchUsers({
     throw error;
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    const userThreads = await Thread.find({ author: userId });
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
+  }
+}
