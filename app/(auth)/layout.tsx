a9bdb412eedd3f19354ebbd8f5ed288a56225fed
Pakzad <pakzadlive@gmail.com>
2023-11-05 13:30:14 +0100

    The login section placed in the center

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center min-h-screen">{children}</div>
+        </body>
       </html>
     </ClerkProvider>
   );
